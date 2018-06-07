caf5f03ed45285ed75c3215dff27ba142380075c
Michael Swan <rmichaelswan@gmail.com>
2018-06-07 00:35:27 -0700

    Built a simple single perceptron neural network capable of learning very simple things such as boolean AND or OR.

---
+++ CS-booleanNeuralNet/booleanNeuralNet/Perceptron.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace booleanNeuralNet
+{
+  class Perceptron
+  {
+    const double trainingCoeff = 0.01;
+    double[] weights;
+
+    public Perceptron(int inputSize)
+    {
+      Random random = new Random();
+      weights = new double[inputSize];
+      for (int i = 0; i< inputSize; i++)
+      {
+        weights[i] = (random.NextDouble() * 2) - 1;
+      }
+    }
+
+    //given a set of inputs, output the perceptron's prediction based on its current weights.
+    public int FeedForward(int[] inputs)
+    {
+      //if input size different than weight size, you have a problem
+      double result = 0;
+      for (int i = 0; i < inputs.Length; i++)
+      {
+        result += (inputs[i] * weights[i]);
+      }
+      return Activate(result);
+    }
+
+    //return simple perceptron output given a double
+    int Activate(double sum)
+    {
+      if (sum > 0)
+      {
+        return 1;
+      }
+      return 0;
+    }
+
+
+    public void Train(int[] inputs, int desired)
+    {
+      int guess = FeedForward(inputs);
+      double error = desired - guess;
+      for (int i = 0; i < inputs.Length; i++)
+      {
+        weights[i] += trainingCoeff * inputs[i] * error;
+      }
+    }
+
+    public void PrintWeights()
+    {
+      for(int i = 0; i < weights.Length; i++)
+      {
+        Console.Write("Weight {0}: {1}. ",i,weights[i]);
+      }
+      Console.Write("\n\n");
+    }
+  }
+}
--- CS-booleanNeuralNet/booleanNeuralNet/Program.cs
+++ CS-booleanNeuralNet/booleanNeuralNet/Program.cs
@@ -7,41 +7,50 @@ namespace booleanNeuralNet
     static void Main(string[] args)
     {
       double desiredResult = 1;
-      double learningRate = 0.2;
-      double[] inputs = new double[] { 1, 1 };
-      double[] weights = new double[] { 0.5, -1 };
-      double result = EvalNeuralNet(inputs, weights);
-      int neuronOut = Active(result);
-      EvalError(result, desiredResult);
+      Perceptron p = new Perceptron(3);
+      //double[] input = { 1, 1, 1 }; //bool1, bool2, bias
+      //int result = p.FeedForward(input);
+      //Console.WriteLine("Output of percecptron was: {0}", result);
+      //EvalError(result, desiredResult);
       //Console.WriteLine("Result1: {0}. Result2: {1}.",result,result2);
+      Trainer(10000,p);
       Console.ReadKey();
     }
-    //TODO - make real perceptron.
 
-
-    //garbage function based on garbage article from garbage frontend developer
-    static double EvalNeuralNet(double[] inputs, double[] weights)
+    //Train to know the logical AND function
+    static void Trainer(int runs, Perceptron perc)
     {
-      double result = 0;
-      for(int i = 0; i < inputs.Length; i++)
+      Random rand = new Random();
+      int[] inputs = { 0, 0, 0 };
+      int answer = 0;
+      int result = 0;
+      int correctIndex = 0;
+      int wrongIndex = 0;
+      for (int i = 0; i < runs; i++)
       {
-        result += (inputs[i] * weights[i]);
+        inputs[0] = rand.Next() % 2;
+        inputs[1] = rand.Next() % 2;
+        inputs[2] = 1; //bias
+        answer = (inputs[0] | inputs[1]); //current can only do & and |. XOR(^) does not work as it is not linearly separable
+        result = perc.FeedForward(inputs);
+        if (result == answer)
+        {
+          correctIndex = i;
+          Console.WriteLine("Run{0}-Inputs: {1}, {2}. Results: {3}", i, inputs[0], inputs[1], result);
+        }
+        else
+        {
+          wrongIndex = i;
+          Console.WriteLine("Run{0}-Inputs: {1}, {2}. Results (WRONG): {3}", i, inputs[0], inputs[1], result);
+        }
+        perc.PrintWeights();
+        if (correctIndex > wrongIndex + 100) //stop if it gets it right 100 times in a row
+        {
+          break;
+        }
+        perc.Train(inputs, answer);
       }
-      return result;
     }
 
-    static int Active(double sum)
-    {
-      if (sum > 0)
-      {
-        return 1;
-      }
-      return -1;
-    }
-
-    static double EvalError(double output, double answer)
-    {
-      return answer - output;
-    }
   }
 }
